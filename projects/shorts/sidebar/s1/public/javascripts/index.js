b129ac76577d962e1f0df64413d52848bde9226a
laranjadev <far820320@gmail.com>
2021-10-12 20:53:25 -0300

    20:53

--- projects/shorts/sidebar/s1/public/javascripts/index.js
+++ projects/shorts/sidebar/s1/public/javascripts/index.js
@@ -1,10 +1,9 @@
 const list = window.document.querySelectorAll('.list');
-function activeLink () {
-    list.forEach((item) => {
-        item.classList.remove('active');
-    });
-    this.classList.add('active');
-}
 list.forEach((item) => {
-    item.addEventListener('click', activeLink);
+    item.addEventListener('click', function () {
+        list.forEach((item) => {
+            item.classList.remove('active');
+        });
+        this.classList.add('active');
+    });
 });
